08495737c5f5068db4c1b2d45811a6babd807480
cheikhbdh <22003@supnum.mr>
2024-03-25 08:20:39 +0000

    lkl

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import LandingPage from './components/pages/landing'; // Import LandingPage component
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+root.render(
+  <>
+
+    <LandingPage />
+  </>
+);
